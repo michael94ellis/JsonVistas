daff6330d2fb6d29bc5a5c52fc4357addf96a110
Michael Ellis <michael94ellis@gmail.com>
2020-03-22 12:53:07 -0400

    Drag and Drop circles

--- JsonVistas/ContentView.swift
+++ JsonVistas/ContentView.swift
@@ -9,9 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        HStack {
+            HStack {
+                ScreenArea()
+            }
+            VStack {
+                Text("Output")
+                Text("V:|-(example)-|")
+                Text("H:|-(example)-|")
+            }
+            .frame(width: 100, height: 200, alignment: .leading)
+            .padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 0))
+        }.padding(50)
+    }
+}
+
+struct DraggableCircle: View {
+    
+    @State private var currentPosition: CGSize = .zero
+    @State private var newPosition: CGSize = .zero
+    
+    var body: some View {
+        Circle()
+            .frame(width: 100, height: 100)
+            .foregroundColor(Color.red)
+            .offset(x: self.currentPosition.width, y: self.currentPosition.height)
+            .gesture(DragGesture()
+                .onChanged { value in
+                    self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: value.translation.height + self.newPosition.height)
+            }
+                .onEnded { value in
+                    self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: value.translation.height + self.newPosition.height)
+                    print(self.newPosition.width)
+                    self.newPosition = self.currentPosition
+                }
+        )
+    }
+}
+
+struct ScreenArea: View {
+    
+    var body: some View {
+        VStack {
+            DraggableCircle()
+            DraggableCircle()
+        }
+        .frame(width: 400, height: 400, alignment: .leading)
+        .background(Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 414, height: 736))
+    }
+}
+
+struct MyDropDelegate: DropDelegate {
+    func performDrop(info: DropInfo) -> Bool {
+        return true
     }
 }
 
