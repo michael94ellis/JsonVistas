88704614e9e5bab0182fdbea8957b6bba9c62b52
Michael Ellis <michael94ellis@gmail.com>
2020-03-25 10:44:06 -0400

    array of views

--- JsonVistas/ContentView.swift
+++ JsonVistas/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State public var views = 0
+    @State public var views: [DraggableView] = []
+    private var newDragView: DraggableView {
+        DraggableView(boundedBy: CGRect(x: 0, y: 0, width: Int(self.size.width), height: Int(self.size.height)))
+    }
     var dropDelegate: DraggableViewDropDelegate
     @State private var size: CGSize = CGSize(width: 414, height: 736)
     
@@ -19,16 +22,16 @@ struct ContentView: View {
     }
     
     func addView() {
-        self.views += 1
+        self.views.append(newDragView)
         print("YEE HAW")
     }
     
     var body: some View {
         HStack {
             ZStack {
-                if views > 0 {
-                    ForEach(0..<views, id: \.self) { index in
-                        DraggableView(boundedBy: CGRect(x: 0, y: 0, width: Int(self.size.width), height: Int(self.size.height)))
+                if !views.isEmpty {
+                    ForEach(self.views.indices, id: \.self) { index in
+                        self.views[index]
                             .position(x: 0, y: 0)
                     }
                 }
