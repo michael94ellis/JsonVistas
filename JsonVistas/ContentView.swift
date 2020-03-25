d24cbe9a44ca71e2580c21876a0bd3dbb2497bc0
Michael Ellis <michael94ellis@gmail.com>
2020-03-24 20:51:10 -0400

    single struct

--- JsonVistas/ContentView.swift
+++ JsonVistas/ContentView.swift
@@ -11,18 +11,35 @@ import SwiftUI
 struct ContentView: View {
     
     @State var draggingItemLocation: CGPoint = .zero
-    var screenArea: ScreenArea?
+    @State public var images: [String] = []
+    var dropDelegate: DraggableViewDropDelegate
+    @State private var size: CGSize = CGSize(width: 414, height: 736)
     
     init() {
-        self.screenArea = ScreenArea(display: $draggingItemLocation)
+        dropDelegate = DraggableViewDropDelegate()
+    }
+    
+    func addView() {
+        self.images.append("textfield")
+        print("YEE HAW")
     }
     
     var body: some View {
         HStack {
             HStack {
-                self.screenArea
+                VStack {
+                    ForEach(images, id: \.self) { viewImage in
+                        DraggableView(boundedBy: CGRect(x: -50, y: 0, width: self.size.width, height: self.size.height), location: self.$draggingItemLocation)
+                    }
+                }
+                .frame(width: size.width, height: size.height)
+                .background(Rectangle().fill(Color.blue))
+                .onDrop(of: ["drag.circle"], delegate: dropDelegate)
             }
             VStack {
+                Button("Add View", action: {
+                    self.addView()
+                })
                 Text("Current Position")
                 Text("X: \(draggingItemLocation.x)")
                 Text("Y: \(draggingItemLocation.y)")
@@ -35,42 +52,7 @@ struct ContentView: View {
         }.padding(50)
     }
 }
-struct ScreenArea: View {
-    
-    @State public var draggableViews: [String] = []
-    
-    var displayPoint: Binding<CGPoint>
-    var dropDelegate: DraggableViewDropDelegate
-    @State private var size: CGSize = CGSize(width: 414, height: 736)
-    
-    init(display: Binding<CGPoint>) {
-        displayPoint = display
-        dropDelegate = DraggableViewDropDelegate()
-    }
-    
-    func addView() {
-        self.draggableViews.append("DV")
-        print("YEE HAW")
-    }
-    
-    func buildView() -> DraggableView {
-       return DraggableView(boundedBy: CGRect(x: -50, y: 0, width: self.size.width, height: self.size.height), location: self.displayPoint)
-    }
-    
-    var body: some View {
-        VStack {
-            Button("Add View", action: {
-                self.addView()
-            })
-            ForEach(draggableViews, id: \.self) { view in
-                self.buildView()
-            }
-        }
-        .frame(width: size.width, height: size.height)
-        .background(Rectangle().fill(Color.blue))
-        .onDrop(of: ["drag.circle"], delegate: dropDelegate)
-    }
-}
+
 
 class DraggableViewDropDelegate: DropDelegate {
     func performDrop(info: DropInfo) -> Bool {
